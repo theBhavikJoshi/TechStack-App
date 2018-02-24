a53b326b804c8ff41c6496b429e058e42ad0e881
Bhavik Joshi <bhavikjoshi29@gmail.com>
2018-02-24 20:31:58 +0530

    Redux Boilerplate Ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers/index';
 
 const App = () => {
 	return (
-		<View>
-			APP
-		</View>
+		<Provider store={createStore(reducers)}>
+			<View />
+		</Provider>
 	)
 }
 
